62ad594d4271cfe2197eac63547ff2d3bc78eb3b
Sam Buchl <buchl.sam@gmail.com>
2021-02-28 00:06:10 -0600

    add first templates: nav menu and index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sam'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Mantorville!'
+        },
+        {
+            'author': {'username': 'Hannah'},
+            'body': 'Money Heist was such a cool show!'
+        }
+    ]
+    return render_template('index.html', title='Poolside', user=user, posts=posts)
