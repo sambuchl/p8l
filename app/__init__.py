fd9233027890a7a8e5481ec26cc3466600360e05
Sam Buchl <buchl.sam@gmail.com>
2021-04-11 16:42:45 -0500

    add date-time formatting with flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
 from config import Config
+from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
